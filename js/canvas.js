c7a4b2c4a3cc9c1e931b07d1d682ce943e684171
sscarduzio <scarduzio@gmail.com>
2014-11-11 16:30:53 -0800

    Canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,40 +1,62 @@
-window.onload = function() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var width  = window.innerWidth;
-    var height = window.innerHeight;
-    canvas.height = height;
-    canvas.width = width;
-    canvas.addEventListener('mousedown', function(e) {
-      this.down = true;  
-      this.X = e.pageX ;
-      this.Y = e.pageY ;
-      this.color = rgb();
-    }, 0);
-    canvas.addEventListener('mouseup', function() {
-      this.down = false;      
-    }, 0);
-    canvas.addEventListener('mousemove', function(e) {
-      this.style.cursor = 'pointer';
-      if(this.down) {
-          ctx.beginPath();
-          ctx.moveTo(this.X, this.Y);
-          ctx.lineCap = 'round';
-           ctx.lineWidth = 3;
-          ctx.lineTo(e.pageX , e.pageY );
-          ctx.strokeStyle = this.color;
-          ctx.stroke();
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
 
-         this.X = e.pageX ;
-         this.Y = e.pageY ;
-      }
-    }, 0);
+function InitThis() {
+    ctx = document.getElementById('myCanvas').getContext("2d");
 
-    function rgb() {
-      color = 'rgb(';
-      for(var i = 0; i< 3; i++) {
-        color += Math.floor(Math.random() * 255)+',';
-      }
-      return color.replace(/\,$/,')');
-    }    
-  };
+    $('#myCanvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#myCanvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#myCanvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+      $('#myCanvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = $('#selColor').val();
+        ctx.lineWidth = $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+  
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+$( document ).ready(function() {
+  var img = $("#myImage")
+  img.load( function(){
+      var canv = $("#myCanvas")
+      
+      var x = img.offset().left 
+      var y = img.offset().top
+
+      var w = img.width()
+      var h = img.height()
+
+//      canv.css({ width: w, height: h})
+       InitThis()    
+  })
+
+});
