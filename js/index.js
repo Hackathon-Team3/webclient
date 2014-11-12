2da0bc523b193af1b52439a8d53d04827a2912e1
Milen Pavlov <mpavlov@amplify.com>
2014-11-11 16:23:00 -0800

    mars location page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-function submit_action() {
+function submit_action(latlong) {
   submit_url = "index.html";
-  redirect_url = "http://maps.google.com/mars";
+  redirect_url = "mars_image_location.html";
 
   $.post(submit_url, function(data) {
     window.location.href = redirect_url;
   })
   .fail(function() {
     console.log("Failed to call " + submit_url);
+    alert("500 error :(");
   })
 }
