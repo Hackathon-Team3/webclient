bf920f048038d9a6a07b13bd5ec668ac4cf51380
Milen Pavlov <mpavlov@amplify.com>
2014-11-11 19:24:05 -0800

    commented out fake rest api call for s3 compatibility, until rest api arrives

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ function submit_action(latitude, longitude) {
   submit_url = "index.html";
   redirect_url = "mars_image_location.html?latitude=" + latitude + "&longitude=" + longitude;
 
-  $.post(submit_url, function(data) {
+  /*$.post(submit_url, function(data) {
     window.location.href = redirect_url;
   })
   .fail(function() {
     console.log("Failed to call " + submit_url);
     alert("500 error :(");
   })
+  */
+  window.location.href = redirect_url;
 }
